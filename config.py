2ab8d43e34ac084efe853c85db03d46c70feaeba
ossy793 <okorieosinakachi@gmail.com>
2025-12-05 22:11:25 +0100

    implement hospital check-in

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from datetime import timedelta
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///hospital.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-key'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
